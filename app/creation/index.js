0c3eaaebceab6843b5ce0590f302532f258d5d93
WEISU <jacketsu@hotmail.com>
2017-08-10 13:26:24 -0400

    new Componenet

--- app/creation/index.js
+++ app/creation/index.js
@@ -7,6 +7,8 @@
 var ReactNative = require('react-native')
 var React = require('react')
 import Icon from 'react-native-vector-icons/Ionicons'
+var request = require('../common/request')
+var config = require('../common/config')
 
 var StyleSheet = ReactNative.StyleSheet
 var Text = ReactNative.Text
@@ -15,8 +17,16 @@ var ListView = ReactNative.ListView
 var TouchableHighlight = ReactNative.TouchableHighlight
 var Image = ReactNative.Image
 var Dimensions = ReactNative.Dimensions
+var RefreshControl = ReactNative.RefreshControl
+var ActivityIndicatorIOS = ReactNative.ActivityIndicatorIOS
 
 var width = Dimensions.get('window').width
+
+var cachedResults = {
+	nextPage: 1,
+	items: [],
+	total: 0
+}
 // import React, { Component } from 'react';
 // import {
 //   AppRegistry,
@@ -25,6 +35,55 @@ var width = Dimensions.get('window').width
 //   View
 // } from 'react-native';
 
+var Item = React.createClass({
+	getInitialState() {
+		var row = this.props.row
+
+		return {
+			row: row
+		}
+	}
+	
+	render() {
+		return(
+		  	<TouchableHighlight>
+		  		<View style={styles.item}>
+		  			<Text style={styles.title}>{row.title}</Text>
+		  			<Image
+		  				source={{uri: row.thumb}}
+		  				style={styles.thumb}
+		  			>
+		  				<Icon
+					    	name='ios-play'
+					    	size={28}
+					    	style={styles.play}
+					    />
+		  			</Image>
+		  			<View style={styles.itemFooter}>
+		  				<View style={styles.handleBox}>
+		  					<Icon
+						    	name='ios-heart-outline'
+						    	size={28}
+						    	style={styles.up}
+						    />
+		  					<Text style={styles.handleText}>like</Text>
+		  				</View>
+		  				<View style={styles.handleBox}>
+		  					<Icon
+						    	name='ios-chatbubbles-outline'
+						    	size={28}
+						    	style={styles.commentIcon}
+						    />
+		  					<Text style={styles.handleText}>comment</Text>
+		  				</View>
+		  			</View>
+
+		  		</View>
+		  	</TouchableHighlight>
+		)
+	}
+})
+
 var List = React.createClass({
   getInitialState: function() {
 	var ds = new ListView.DataSource({
@@ -32,6 +91,8 @@ var List = React.createClass({
 	})
 
 	return {
+		isLoadingTail: false,
+		isRefreshing: false,
 	    dataSource: ds.cloneWithRows([{
         "_id":"230000199110204818","thumb":"https://facebook.github.io/react/img/logo_og.png","title":"测试内容y22c","video_address":"http://www.imooc.com/video/10427"
     }
@@ -58,43 +119,112 @@ var List = React.createClass({
 	} 
   },
   
-  renderRow: function(row) {
-  	return (
-	  	<TouchableHighlight>
-	  		<View style={styles.item}>
-	  			<Text style={styles.title}>{row.title}</Text>
-	  			<Image
-	  				source={{uri: row.thumb}}
-	  				style={styles.thumb}
-	  			>
-	  				<Icon
-				    	name='ios-play'
-				    	size={28}
-				    	style={styles.play}
-				    />
-	  			</Image>
-	  			<View style={styles.itemFooter}>
-	  				<View style={styles.handleBox}>
-	  					<Icon
-					    	name='ios-heart-outline'
-					    	size={28}
-					    	style={styles.up}
-					    />
-	  					<Text style={styles.handleText}>like</Text>
-	  				</View>
-	  				<View style={styles.handleBox}>
-	  					<Icon
-					    	name='ios-chatbubbles-outline'
-					    	size={28}
-					    	style={styles.commentIcon}
-					    />
-	  					<Text style={styles.handleText}>comment</Text>
-	  				</View>
-	  			</View>
+  _renderRow: function(row) {
+  	return <Item row={row} />
+  },
 
-	  		</View>
-	  	</TouchableHighlight>
-	)
+  componentDidMount: function() {
+  	this._fetchData(1)
+  },
+
+  _fetchData: function (page){
+  	var that = this
+
+  	if (page !== 0) {
+  		this.setState({
+  			isLoadingTail: true
+  		})
+  	} else {
+  		this.setState({
+	  		isRefreshing: true
+	  	})
+  	}
+
+    request.get(config.api.base + config.api.creations, {
+      accessToken: 'a',
+      page: page
+    })
+      .then((data) => {
+      	if (data.success) {
+      		var items = cachedResults.items.slice()
+
+      		if (page !== 0) {
+      			items = items.concat(data.data)
+      			cachedResults.nextPage += 1
+      		} else {
+      			items = data.data.concat(items)
+      		}
+
+      		cachedResults.items = items
+      		cachedResults.total = data.total
+
+      		setTimeout(function() {
+      			if (page !== 0) {
+      				that.setState({
+		      			isLoadingTail: false,
+		      			dataSource: this.state.dataSource.cloneWithRows(cachedResults.items)
+		      		})
+      			} else {
+      				that.setState({
+		      			isRefreshing: false,
+		      			dataSource: this.state.dataSource.cloneWithRows(cachedResults.items)
+		      		})
+      			}
+	      	}, 20)
+      	}
+      })
+      .catch((error) => {
+      	if (page !== 0) {
+      		this.setState({
+      			isLoadingTail: false
+      		})
+      	} else {
+      		this.setState({
+      			isRefreshing: false
+      		})
+      	}
+        console.error(error);
+      })
+  },
+
+  _hasMore() {
+  	return cachedResults.items.length !== cachedResults.total
+  },
+
+  _fetchMoreData() {
+  	if (!this._hasMore() || this.state.isLoadingTail) {
+  		return
+  	}
+
+  	var page = cachedResults.nextPage
+
+  	this._fetchData(page)
+  },
+
+  _renderFooter() {
+  	if (!this._hasMore() && cachedResults.total != 0) {
+  		return (
+  			<View style={styles.loadingMore}>
+  				<Text style={styles.loadingText}No more</Text>
+  			</View>
+  		)
+  	}
+
+  	if (!this.state.isLoadingTail) {
+  		return <View style={styles.loadingMore} />
+  	}
+
+  	return <ActivityIndicatorIOS
+  		style={[styles.centering, {height: 80}]}
+  	/>
+  },
+  
+  _onRefresh() {
+  	if (!this._hasMore() || this.state.isRefreshing) {
+  		return
+  	}
+
+  	this._fetchData(0)
   },
 
   render: function() {
@@ -105,8 +235,20 @@ var List = React.createClass({
       	</View>
       	<ListView
       		dataSource={this.state.dataSource}
-      		renderRow={this.renderRow}
+      		renderRow={this._renderRow}
+      		renderFooter={this._renderFooter}
+      		onEndReached={this._fetchMoreData}
+      		refreshControl={
+      			<RefreshControl
+		           refreshing={this.state.isRefreshing}
+		           onRefresh={this._onRefresh}
+		           tintColor="#ff6600"
+		           title="Loading..."
+		        />
+      		}
+      		onEndReachedThreshold={20}
       		enableEmptySections={true}
+      		showsVerticalScrollIndicator={false}
       		automaticallyAdjustContentInsets={false}
       	/>
       </View>
@@ -194,8 +336,16 @@ var styles = StyleSheet.create({
   commentIcon: {
   	fontSize: 22,
   	color: '#333'
-  }
-  
+  },
+
+  loadingMore: {
+  	marginVertical: 20
+  },
+
+  loadingText: {
+  	color: '#777',
+  	textAlign: 'center'
+  }  
 })
 
 module.exports = List
